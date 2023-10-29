b82dc4b4adf3f54924e75fb5c0a5480103834082
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-29 11:36:15 +0530

    unary operator overloading in c++

--- file.cpp
+++ file.cpp
@@ -1,47 +1,70 @@
-// operator overloading using member and friend function in c++.
-// return total sum of students marks and return which student have greater marks.
 #include<iostream>
+#include<vector>
 using namespace std;
 
-class student {
-    int subj1Marks;
-    int subj2Marks;
-    int sum;
+class PopulationMatrix {
+    int m;
+    vector<vector<int>> vec;
 public:
-    student(int subj1Marks, int subj2Marks)
-    {
-        this->subj1Marks = subj1Marks;
-        this->subj2Marks = subj2Marks;
-        sum = subj1Marks + subj2Marks;
+    PopulationMatrix() {
+        cin >> m;
+        for(int i = 0; i < m; i++)
+        {
+            vec.push_back(vector<int>());
+            for(int j = 0; j < m; j++)
+            {
+                int ele;
+                cin >> ele;
+                vec[i].push_back(ele);
+            }
+        }
     }
-    student(int total)
+    void operator--()
     {
-        this->sum = total;
+        vector<vector<int>> vec2 = this->vec;
+        for(int i = 0; i < this->m; i++)
+        {
+            for(int j = 0; j < this->m; j++)
+            {
+                vec2[i][j]--;
+            }
+        }
+        cout << "Matrix after incrementing each element by 1:" << endl;
+        for(int i = 0; i < this->m; i++)
+        {
+            for(int j = 0; j < this->m; j++)
+            {
+                cout << vec2[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
-    void display()
+    void operator++()
     {
-        cout << "Sum of student 1 and student 2 marks is " << this->sum << endl;
-    }
-    friend student operator+(student &stu1, student &stu2)
-    {
-        cout << "Student 1 total marks is " << stu1.sum << endl;
-        cout << "Student 2 total marks is " << stu2.sum << endl;
-        return student(stu1.subj1Marks + stu1.subj2Marks + stu2.subj1Marks + stu2.subj2Marks);
-    }
-    friend bool operator>(student &stu1, student &stu2)
-    {
-        return stu1.sum > stu2.sum ? 1 : 0;
+        vector<vector<int>> vec2 = this->vec;
+        for(int i = 0; i < this->m; i++)
+        {
+            for(int j = 0; j < this->m; j++)
+            {
+                vec2[i][j]++;
+            }
+        }
+        cout << "Matrix after decrementing each element by 1:" << endl;
+        for(int i = 0; i < this->m; i++)
+        {
+            for(int j = 0; j < this->m; j++)
+            {
+                cout << vec2[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
 };
 
 int main(){
-    student stu1(10, 15), stu2(10, 13);
-
-    student obj = stu1 + stu2;
-    obj.display();
-
-    if(stu1 > stu2) cout << "Student 1 total marks is greater than student 2 marks" << endl;
-    else cout << "student 2 marks is greater than student 1 marks " << endl;
-
+    PopulationMatrix pm;
+    ++pm;
+    --pm;
+    
     return 0;
 }
